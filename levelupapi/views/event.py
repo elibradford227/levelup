4b9ae787d51edd8f7cf969e4582776c7f178c7e1
Eli Bradford <elibradford227@gmail.com>
2023-12-02 14:01:43 -0600

    add and leave event on client side working

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -30,13 +30,14 @@ class EventView(ViewSet):
         events = Event.objects.all()
         uid = request.META['HTTP_AUTHORIZATION']
         gamer = Gamer.objects.get(uid=uid)
+
         
         for event in events:
             # Check to see if there is a row in the Event Games table that has the passed in gamer and event
             event.joined = len(EventGamer.objects.filter(
             gamer=gamer, event=event)) > 0
-    
         serializer = EventSerializer(events, many=True)
+        print(uid, gamer.__dict__)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def create(self, request):
@@ -80,8 +81,10 @@ class EventView(ViewSet):
     @action(methods=['post'], detail=True)
     def signup(self, request, pk):
         """Post request for a user to sign up for an event"""
+        
+        uid = request.META['HTTP_AUTHORIZATION']
 
-        gamer = Gamer.objects.get(uid=request.data["user_id"])
+        gamer = Gamer.objects.get(uid=uid)
         event = Event.objects.get(pk=pk)
         attendee = EventGamer.objects.create(
             gamer=gamer,
@@ -92,8 +95,8 @@ class EventView(ViewSet):
     @action(methods=['delete'], detail=True)
     def leave(self, request, pk):
         """Delete request for a user to leave an event"""
-
-        gamer = Gamer.objects.get(uid=request.data["user_id"])
+        uid = request.META['HTTP_AUTHORIZATION']
+        gamer = Gamer.objects.get(uid=uid)
         event = Event.objects.get(pk=pk)
         eventgamer = EventGamer.objects.filter(gamer_id=gamer, event_id=event)
         eventgamer.delete()
